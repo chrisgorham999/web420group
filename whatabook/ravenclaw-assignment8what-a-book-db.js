141a6bc4e8ab66ac6c0c4a03157ad76326e33416
Shane Hingtgen <shanehingtgen@gmail.com>
2023-05-03 17:17:15 -0500

    backing up

--- whatabook/ravenclaw-assignment8what-a-book-db.js
+++ whatabook/ravenclaw-assignment8what-a-book-db.js
@@ -12,3 +12,150 @@
     Web-420 GitHub Examples
 =================================================================================================================
 */
+
+// create the books collection
+db.createCollection("books", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        title: {
+          bsonType: "string",
+        },
+        genre: {
+          bsonType: "string",
+        },
+        author: {
+          bsonType: "string",
+        },
+        bookId: {
+          bsonType: "string",
+        },
+      },
+    },
+  },
+});
+
+// create the customer collection
+db.createCollection("customers", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        firstName: {
+          bsonType: "string",
+        },
+        lastName: {
+          bsonType: "string",
+        },
+        customerId: {
+          bsonType: "string",
+        },
+        wishList: {
+          bsonType: "array",
+        },
+      },
+    },
+  },
+});
+
+// Books.
+whereTheRedFernGrows = {
+  title: "Where the Red Fern Grows",
+  genre: "autobiographical fiction",
+  author: "Wilson Rawls",
+  bookId: "0-440-22814-X",
+};
+
+postOffice = {
+  title: "Post Office",
+  genre: "Drama",
+  author: "Charles Bukowski",
+  bookId: "1039104810481",
+};
+
+factotum = {
+  title: "Factotum",
+  genre: "Drama",
+  author: "Charles Bukowski",
+  bookId: "10490124910",
+};
+
+theCruelPrince = {
+  title: "The Cruel Prince",
+  genre: "Fantasy",
+  author: "Holly Black",
+  bookId: "9780316310277",
+};
+
+// inserting our documents to the books collection
+db.books.insertOne(whereTheRedFernGrows);
+db.books.insertOne(postOffice);
+db.books.insertOne(factotum);
+db.books.insertOne(theCruelPrince);
+
+// customers
+shane = {
+  firstName: "Shane",
+  lastName: "Hingtgen",
+  customerId: "12345",
+  wishList: [
+    {
+      title: "Where the Red Fern Grows",
+      genre: "autobiographical fiction",
+      author: "Wilson Rawls",
+      bookId: "0-440-22814-X",
+    },
+    {
+      title: "Factotum",
+      genre: "Drama",
+      author: "Charles Bukowski",
+      bookId: "10490124910",
+    },
+  ],
+};
+
+chris = {
+  firstName: "Chris",
+  lastName: "Gorham",
+  customerId: "1091904014",
+  wishList: [
+    {
+      title: "Where the Red Fern Grows",
+      genre: "autobiographical fiction",
+      author: "Wilson Rawls",
+      bookId: "0-440-22814-X",
+    },
+    {
+      title: "The Cruel Prince",
+      genre: "Fantasy",
+      author: "Holly Black",
+      bookId: "9780316310277",
+    },
+  ],
+};
+
+cait = {
+  firstName: "Cait",
+  lastName: "Johnson",
+  customerId: "16136",
+  wishList: [
+    {
+      title: "The Cruel Prince",
+      genre: "Fantasy",
+      author: "Holly Black",
+      bookId: "9780316310277",
+    },
+    {
+      title: "Post Office",
+      genre: "Drama",
+      author: "Charles Bukowski",
+      bookId: "1039104810481",
+    },
+  ],
+};
+
+// inserting our documents to the customers collection
+db.customers.insertOne(shane);
+db.customers.insertOne(chris);
+db.customer.insertOne(cait);
