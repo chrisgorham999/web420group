e1e2fbcd1cf5acbc8f4509357fba8d7ccf3a1cea
Shane Hingtgen <shanehingtgen@gmail.com>
2023-05-03 16:14:28 -0500

    Create ravenclaw-assignment8what-a-book-db.js

---
+++ whatabook/ravenclaw-assignment8what-a-book-db.js
@@ -0,0 +1,14 @@
+/*
+==============================================================================
+; Title: ravenclaw-assignment8what-a-book-db.js
+; Author: House RavenClaw
+; Modified by: Chris Gorham, Caitlynne Johnson, Shane hingtgen
+; Bellevue University
+; Date: 05/03/2023
+; Description: Group project for What a Book week 2
+; Work Cited: 
+    Coding Guidelines
+    Web-420 Assign_8
+    Web-420 GitHub Examples
+=================================================================================================================
+*/
